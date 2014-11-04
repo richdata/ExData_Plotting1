a93af76dfd90c7494487f0be628eda9a7ec4e3cd
Rich Sodon <rich.sodon@verizon.com>
2014-11-04 15:07:54 -0500

    Updated plots 1 & 2 to remove the background
    PNGs typically dont have background colors.  Removed the background
    colors

--- plot1.R
+++ plot1.R
@@ -1,14 +1,20 @@
 
 library(sqldf)
+
+#Read the data
 data<- read.csv.sql("household_power_consumption.txt", 
                     sql = 'select * from file 
                     where (Date == "1/2/2007") | (Date == "2/2/2007")',
                     header = TRUE, sep = ";") 
 
+#prepare the graphics device
+png (file = "plot1.png",width = 480, height = 480, units = "px", bg=NA)
 
+
+#plot the data
 hist (data$Global_active_power, col="red", main = "Global Active Power", 
       xlab="Global Active Power (kilowatts)", ylab="Frequency")
 
 
-dev.copy(png, file = "plot1.png",width = 480, height = 480, units = "px", bg=NA)
+#turn off the graphics device
 dev.off ()
--- plot2.R
+++ plot2.R
@@ -1,14 +1,23 @@
 
 library(sqldf)
+
+#read the data
 data<- read.csv.sql("household_power_consumption.txt", 
                     sql = 'select * from file 
                     where (Date == "1/2/2007") | (Date == "2/2/2007")',
                     header = TRUE, sep = ";") 
 
+
+#Clean the data to add a time stamp
 data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+#open the graphics device
+png (file = "plot2.png",width = 480, height = 480, units = "px", bg=NA)
+
+#crate the plot
 plot (data$DateTime, data$Global_active_power,  type="l", 
       xlab="", ylab="Global Active Power (kilowatts)")
 
 
-dev.copy(png, file = "plot2.png",width = 480, height = 480, units = "px", bg=NA)
+#Turn off the graphics device
 dev.off ()
